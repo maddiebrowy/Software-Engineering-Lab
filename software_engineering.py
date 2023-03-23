3f11e1d9a9c7ae726e17a4ca8c34149b99778c2d
Madeline Browy <browym@ufl.edu>
2023-03-22 20:59:04 -0400

    added menu option 3 and comments

--- software_engineering.py
+++ software_engineering.py
@@ -10,19 +10,19 @@ def main():
         print("1. Encode\n2. Decode\n3. Quit")
         print()
         user_selection = int(input("Please enter an option: "))
-        # user selection 1
+        # menu option 1
         if user_selection == 1:
             password = input("Please enter your password to encode: ")
             password = [int(i) for i in password]
             print("Your password has been encoded and stored!")
             print()
-        # user selection 2
+        # menu option 2
         if user_selection == 2:
             new_password = ''.join(str(i) for i in encode(password))
             password = ''.join(str(i) for i in password)
             print(f'The new password is {new_password}, and the original password is {password}.')
             print()
-        # user selection 3
+        # menu option 3
         if user_selection == 3:
             exit()
 
